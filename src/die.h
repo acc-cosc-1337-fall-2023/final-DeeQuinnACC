f0d93d64b0997b236ce8d2cea72ce1ced9cef177
DeeQuinnACC <142953458+DeeQuinnACC@users.noreply.github.com>
2023-12-05 01:19:53 +0000

    Question 1

--- src/die.h
+++ src/die.h
@@ -1,1 +1,24 @@
 //
+#include <stdlib.h>
+
+#ifndef DIE_H
+#define DIE_H
+
+class Die
+{
+public:
+    //Rolls once to generate a value for roll_value
+    Die(){ roll(); }
+    //Randomly assigns a value to roll_value in the range of 1 to int sides
+    void roll();
+    //Returns roll_value
+    int rolled_value() const { return roll_value; }
+
+private:
+    //Stores a randomly assigned value 
+    int roll_value;
+    //Die sides; defaults to 6
+    int sides{6};
+};
+
+#endif
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,29 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "come_out_phase.h"
+#include "die.h"
+#include "phase.h"
+#include "point_phase.h"
+#include "roll.h"
+#include "shooter.h"
+#include <iostream>
+#include <time.h>
 
-TEST_CASE("Verify Test Configuration", "verification") {
+TEST_CASE("Verify Test Configuration", "verification") 
+{
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Test if Die::roll() generates a value between 1 and 6")
+{
+	std::srand(time(NULL));
+	Die d;
+	for(int i=0; i<10; i++)
+	{
+		d.roll();
+		int roll = d.rolled_value();
+		REQUIRE(roll > 0);
+		REQUIRE(roll < 7);
+	}
+	
 }
